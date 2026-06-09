3dffbed60b1365c74e30cbb13a1af2b92d0459b5
Samuel Severino <samuelsev.work2020@gmail.com>
2026-06-09 10:50:32 -0400

    [ADD] helpdesk: modulo helpdesk_mgmt_project_custom para Odoo 16

---
+++ helpdesk_mgmt_project_custom/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Helpdesk Project",
+    "summary": "Add the option to select project in the tickets.",
+    "version": "16.0.1.0.2",   # ← Incrementa la versión
+    "license": "AGPL-3",
+    "category": "After-Sales",
+    "author": "PuntSistemes S.L.U., Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/helpdesk",
+    "depends": ["helpdesk_mgmt", "project"],
+    "data": [
+        "views/helpdesk_ticket_view.xml",
+        "views/helpdesk_ticket_team_view.xml",
+        "views/project_view.xml",
+        "views/project_task_view.xml",
+        "views/project_milestone.xml",
+        "views/menus.xml",
+    ],
+    "development_status": "Production/Stable",
+}
