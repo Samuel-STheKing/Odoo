865e92989786665f06fe3d648547f5f953e6ce5a
Samuel Severino <samuelsev.work2020@gmail.com>
2026-06-09 11:37:28 -0400

    [ADD] helpdesk: modulo helpdesk_mgmt_project_custom para Odoo 16

--- helpdesk_mgmt_project_custom/__manifest__.py
+++ helpdesk_mgmt_project_custom/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Helpdesk Project",
     "summary": "Add the option to select project in the tickets.",
-    "version": "16.0.1.0.2",   # ← Incrementa la versión
+    "version": "16.0.1.0.3",
     "license": "AGPL-3",
     "category": "After-Sales",
     "author": "PuntSistemes S.L.U., Odoo Community Association (OCA)",
--- helpdesk_mgmt_project_custom/models/project.py
+++ helpdesk_mgmt_project_custom/models/project.py
@@ -1,21 +1,28 @@
 from odoo import api, fields, models
+from odoo.tools.translate import _
 
 
 class ProjectProject(models.Model):
     _inherit = "project.project"
 
     ticket_ids = fields.One2many(
-        comodel_name="helpdesk.ticket", inverse_name="project_id", string="Tickets"
+        comodel_name="helpdesk.ticket", 
+        inverse_name="project_id", 
+        string="Tickets"
     )
+    
     ticket_count = fields.Integer(compute="_compute_ticket_count")
+    
     label_tickets = fields.Char(
         string="Use Tickets as",
-        default=lambda self: self.env._("Tickets"),
+        default=lambda self: _("Tickets"),
         translate=True,
         help="Gives label to tickets on project's kanban view.",
     )
+    
     todo_ticket_count = fields.Integer(
-        string="Number of tickets", compute="_compute_ticket_count"
+        string="Number of tickets", 
+        compute="_compute_ticket_count"
     )
 
     @api.depends("ticket_ids", "ticket_ids.stage_id")
@@ -24,15 +31,18 @@ class ProjectProject(models.Model):
         domain = [("project_id", "in", self.ids)]
         fields = ["project_id"]
         groupby = ["project_id"]
+        
         counts = {
             pr["project_id"][0]: pr["project_id_count"]
             for pr in HelpdeskTicket.read_group(domain, fields, groupby)
         }
+        
         domain.append(("closed", "=", False))
         counts_todo = {
             pr["project_id"][0]: pr["project_id_count"]
             for pr in HelpdeskTicket.read_group(domain, fields, groupby)
         }
+        
         for record in self:
             record.ticket_count = counts.get(record.id, 0)
             record.todo_ticket_count = counts_todo.get(record.id, 0)
@@ -43,7 +53,7 @@ class ProjectProject(models.Model):
             buttons.append(
                 {
                     "icon": "life-ring",
-                    "text": self.env._("Tickets"),
+                    "text": _("Tickets"),
                     "number": self.ticket_count,
                     "action_type": "object",
                     "action": "action_open_helpdesk_tickets",
@@ -56,7 +66,7 @@ class ProjectProject(models.Model):
     def action_open_helpdesk_tickets(self):
         self.ensure_one()
         action = self.env["ir.actions.actions"]._for_xml_id(
-            "helpdesk_mgmt_project.ticket_action_from_project"
+            "helpdesk_mgmt_project_custom.ticket_action_from_project"
         )
         action["domain"] = [("project_id", "=", self.id)]
         action["context"] = {
